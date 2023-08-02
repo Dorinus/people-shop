85bc1587a6a8b0815e6c1e885483298e84642761
dorinus <kiradorin94@gmail.com>
2023-08-02 16:26:17 +0200

    BackEnd APIs

--- backend/server.js
+++ backend/server.js
@@ -2,18 +2,28 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+import cookieParser from 'cookie-parser';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+
 
 dotenv.config();
 connectDB(); // Connect to MongoDB
 const app = express();
 const port = process.env.PORT || 5000;
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Cookie parser middleware
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
